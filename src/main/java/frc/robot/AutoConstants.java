7644a69160d8ff2989b13f2ac9cacbb823ab643f
Hayden Schroeder <hayden.schroeder@STEM2U.org>
2020-11-12 10:54:58 -0600

    formatting stuff

---
+++ src/main/java/frc/robot/AutoConstants.java
@@ -0,0 +1,6 @@
+package frc.robot;
+
+public final class AutoConstants 
+{
+    public static final double AUTO_DRIVE_SPEED = 0.5;
+}
---
+++ src/main/java/frc/robot/OIConstants.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+public final class OIConstants 
+{
+    public static final int DRIVER_CONTROLLER = 0;
+    public static final int OPERATOR_CONTROLLER = 1;
+    public static final int A_BUTTON = 1;
+    public static final int B_BUTTON = 2;
+    public static final int X_BUTTON = 3;
+    public static final int Y_BUTTON = 4;
+    public static final int RIGHT_BUMPER = 6;
+    public static final int LEFT_BUMPER = 5;
+    public static final int id_back_button = 7;
+    public static final int id_start_button = 8;
+    public static final int id_left_stick_press = 9;
+    public static final int id_right_stick_press = 10;
+    public static final int left_stick_x = 0;
+    public static final int left_stick_y = 1;
+    public static final int left_trigger = 2;
+    public static final int right_trigger = 3;
+    public static final int right_stick_x = 4;
+    public static final int right_stick_y = 5;
+}
